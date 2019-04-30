739f3015ea9226931617a9caedb6d91126a273fd
CharmFury <36172495+CharmFury@users.noreply.github.com>
2019-04-30 13:39:55 +0200

    Initial commit
    With basic project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './FirstComponent';
+import AppBar from './AppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       {/* <img src={logo} className="App-logo" alt="logo" />
+        <AppBar />
+        {/*<FirstComponent displaytext="Firsst Component Data"/>*/}
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
